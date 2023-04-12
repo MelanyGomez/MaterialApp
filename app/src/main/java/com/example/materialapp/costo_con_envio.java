5b2a599f6fdb248c50fb524d64e44ea91dd5aafe
melany <go404756@uaeh.edu.mx>
2023-04-11 23:18:14 -0600

    Second Commit

--- app/src/main/java/com/example/materialapp/costo_con_envio.java
+++ app/src/main/java/com/example/materialapp/costo_con_envio.java
@@ -2,7 +2,9 @@ package com.example.materialapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class costo_con_envio extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class costo_con_envio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_costo_con_envio);
+    }
+    public void irEditar_costo_materiales(View view){
+        Intent intent = new Intent(costo_con_envio.this,editar_costo_materiales.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
     }
 }
---
+++ app/src/main/java/com/example/materialapp/editar_costo_materiales.java
@@ -0,0 +1,14 @@
+package com.example.materialapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editar_costo_materiales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_costo_materiales);
+    }
+}
