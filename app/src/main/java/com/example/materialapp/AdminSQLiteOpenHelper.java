4308c6de40cd3e5ef2e0c4ab61509027241d428a
melany <go404756@uaeh.edu.mx>
2023-04-20 18:10:38 -0600

    Eight Commit

--- app/src/main/java/com/example/materialapp/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/materialapp/AdminSQLiteOpenHelper.java
@@ -12,8 +12,9 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper{
     @Override
     //el nombre que le asignamos a nuestra base de datos es BaseDeDatos
     public void onCreate(SQLiteDatabase BaseDeDatos) {
-    BaseDeDatos.execSQL("create table materiales(codigo int primary key, nombre text, precio real)");
-
+        BaseDeDatos.execSQL("create table materiales(id_mat integer primary key autoincrement, nombre_mat text(32) not null, precio_mat real(7,3)not null)");
+        BaseDeDatos.execSQL("create table maquinaria(id_maq int primary key, nombre_maq text(32) not null, descripcion_mat text(120) not null, precio_maq_mes real(7,3)not null)");
+        BaseDeDatos.execSQL("create table cliente(id_cli int primary key, nombre_cli text (32) not null, telefono_cli text(15)not null, direccion text(256) not null)");
     }
 
     @Override
--- app/src/main/java/com/example/materialapp/editar_costo_materiales.java
+++ app/src/main/java/com/example/materialapp/editar_costo_materiales.java
@@ -2,10 +2,17 @@ package com.example.materialapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class editar_costo_materiales extends AppCompatActivity {
-
+    public EditText et_nombre_mat, et_costo_mat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
